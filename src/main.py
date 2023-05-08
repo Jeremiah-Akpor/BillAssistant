595395043946b1571ac7cd67f4565144453ba6e9
Jeremiah Akpor <JeremiahAkpor_CS@outlook.com>
2023-05-08 22:27:26 +0200

    started creating bill mode

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 """
 This module contains the `SM` and `MainApp` classes for the BillBuddy app.
 """
-
+import re
 from kivy.uix.screenmanager import ScreenManager
+from kivymd.uix.textfield import MDTextField
 from kivymd.app import MDApp
 
 
@@ -22,7 +23,7 @@ class MainApp(MDApp):
             `SM`: The root `SM` object representing the app UI.
         """
         self.sm = SM()
-        self.sm.current = "Welcome"
+        self.sm.current = "BillMode"
         self.title = "BillBuddy"
         self.icon = "images/icon.jpeg"
         self.theme_cls.theme_style = "Dark"
@@ -39,6 +40,19 @@ class MainApp(MDApp):
             is_dark (`bool`): Whether to switch to dark mode.
         """
         self.theme_cls.theme_style = "Dark" if is_dark else "Light"
+
+    def calculate(self, total_id, money_note: str, amount: MDTextField):
+        try:
+            amount = int(amount.text)
+            total = money_note * amount
+            total_id.text = str(total)
+        except ValueError:
+            # Handle the case where money_note or amount
+            # cannot be converted to integers
+            total_id.text = "Invalid input"
+
+    def focus_next(self, textfield_id):
+        textfield_id.focus = True
 
 
 if __name__ == "__main__":
