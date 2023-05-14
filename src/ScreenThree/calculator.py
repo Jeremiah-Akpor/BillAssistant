d7f1a31339fb6cd53749fb2468239185cd2c0e54
Jeremiah Akpor <JeremiahAkpor_CS@outlook.com>
2023-05-14 13:00:19 +0200

    Finished calculator

--- src/ScreenThree/calculator.py
+++ src/ScreenThree/calculator.py
@@ -5,6 +5,7 @@ from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.screen import MDScreen
 from apputils import load_kv
 
+
 load_kv(__name__)
 
 
@@ -13,4 +14,108 @@ class NavBar(CommonElevationBehavior, MDFloatLayout):
 
 
 class Calculator(MDScreen):
-    pass
+
+    def on_pre_enter(self):
+        self.ids.calc_input.focus = True
+
+    def clear(self):
+        """this function clear the TextInput when the
+        c button is pressed
+        """
+        self.ids.calc_input.text = ""
+        self.ids.answer.text = ""
+
+    # create a button pressing function
+    def button_press(self, num):
+        """
+        updates the TextInput with the numbers
+
+        Args:
+            button (_type_): _description_
+
+        Returns:
+            _type_: _description_
+        """
+        # get the number/s the is already in the TextInput
+        prior = self.ids.calc_input.text
+
+        if "ERROR" in prior:
+            prior = ""
+
+        if prior == "0":
+            self.ids.calc_input.text = f"{num}"
+        else:
+            self.ids.calc_input.text = f"{prior}{num}"
+
+    def math_sign(self, sign):
+        """
+            all the math signs function
+        Returns:
+            _type_: _description_
+        """
+
+        # get the number/s the is already in the TextInput
+        prior = self.ids.calc_input.text
+        if prior.endswith(" / ") or prior.endswith(" * "):
+            self.ids.calc_input.text = f"{prior[:-1]}{sign} "
+        else:
+            # concat the Text box with the math sign
+            self.ids.calc_input.text = f"{prior} {sign} "
+
+    def other_sign(self, sign):
+        """
+            add other signs like dot and negative to the
+            text input
+        Returns:
+            _type_: _description_
+        """
+        # get the number/s the is already in the TextInput
+        prior = self.ids.calc_input.text
+        if sign == "Ans" and self.ids.answer.text != "Invalid Input":
+            self.ids.calc_input.text = self.ids.answer.text
+            self.ids.answer.text = ""
+        elif sign == "Ans" and self.ids.answer.text == "Invalid Input":
+            self.ids.calc_input.text = ""
+            self.ids.answer.text = ""
+        else:
+            if "." in prior.split(" ")[-1] and sign == ".":
+                pass
+            elif prior[0] == "-" and sign == "-":
+                self.ids.calc_input.text = f"{prior[1:]}"
+            elif prior[0] != "-" and sign == "-":
+                self.ids.calc_input.text = f"{sign}{prior}"
+            else:
+                # concat the Text box with the sign
+                self.ids.calc_input.text = f"{prior}{sign}"
+
+    # create equals to function
+
+    def equals(self):
+        """
+        equals function
+        """
+        try:
+            prior = self.ids.calc_input.text
+            answer = eval(prior)  # pylint: disable=eval-used
+            # print answer in the text box
+            self.ids.answer.text = f"{answer}"
+        except (Exception, NameError):  # pylint: disable=broad-except
+            # print answer in the text box
+            self.ids.answer.text = "Invalid Input"
+
+    def backspace(self):
+        """
+        backspace function
+        """
+        prior = self.ids.calc_input.text
+        if "ERROR" in prior:
+            prior = ""
+
+        length = len(prior)
+        if length == 1:
+            self.ids.calc_input.text = "0"
+        elif prior[-1] == " ":
+            self.ids.calc_input.text = f"{prior[:length-2]}"
+        else:
+            self.ids.calc_input.text = f"{prior[:length-1]}"
+            
---
+++ src/hot_reload.py
@@ -0,0 +1,121 @@
+"""
+This is an example using Kaki with Kivy modules.
+"""
+
+from kaki.app import App
+from kivy.clock import Clock
+from kivy.factory import Factory
+
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager
+from kivymd.app import MDApp
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.textfield import MDTextField
+
+from ScreenThree.calculator import Calculator
+
+
+class SM(ScreenManager):
+    """A `ScreenManager` that manages the screens for the BillBuddy app."""
+
+
+class Splash(MDScreen):
+    pass
+
+
+# main app class for kaki app with kivy style
+class Main(MDApp, App):
+    # root app name/title
+    name = "Hot Reload"
+
+    # version number
+    vernum = "0.1.4"
+
+    # window title
+    title = "%s v%s" % (name, vernum)
+
+    # by default it will return to False
+    # set this to True so you can make a change to your .kv file
+    DEBUG = True
+    # *.kv files to watch
+    KV_FILES = [
+        # screen manager
+        "main.kv",
+        # screen
+        "ScreenTwo/bill_mode.kv",
+        "ScreenOne/select_mode.kv",
+        "ScreenThree/calculator.kv",
+        "WelcomeScreen/welcome_screen.kv",
+    ]
+
+    # class to watch
+    CLASSES = {
+        # screen manager
+        "SM": "main",
+        # screens
+        "BillMode": "ScreenTwo.bill_mode",
+        "Welcome": "WelcomeScreen.welcome_screen",
+        "Splash": "main",
+        "SelectMode": "ScreenOne.select_mode",
+        "Calculator": "ScreenThree.calculator",
+    }
+
+    # auto reload path
+    AUTORELOADER_PATHS = [
+        (".", {"recursive": True}),
+    ]
+
+    # build app
+    def build_app(self):
+        self.sm = Factory.SM()
+        self.title = "BillBuddy"
+        self.icon = "images/icon.jpeg"
+        self.sm.current = "Calculator"
+        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_palette = "DeepOrange"
+        self.theme_cls.accent_palette = "BlueGray"
+        self.theme_cls.theme_style_switch_animation = True
+        self.theme_cls.theme_style_switch_animation_duration = 0.95
+
+        FMS = self.sm
+
+        return FMS
+
+    def on_start(self):
+        Clock.schedule_once(self.welcome, 5)
+
+    def welcome(self, *args):  # pylint: disable=unused-argument
+        self.sm.current = "Welcome"
+
+    def toggle_theme(self, is_dark):
+        """Toggle the app's theme.
+
+        Args:
+            is_dark (`bool`): Whether to switch to dark mode.
+        """
+        self.theme_cls.theme_style = "Dark" if is_dark else "Light"
+
+    def calculate(self, total_id, money_note: str, amount: MDTextField):
+        try:
+            amount = int(amount.text)
+            total = money_note * amount
+            total_id.text = str(total)
+        except ValueError:
+            # Handle the case where money_note or amount
+            # cannot be converted to integers
+            if amount.text == "":
+                total_id.text = "0"
+            else:
+                total_id.text = "Invalid input"
+
+    def focus_next(self, textfield_id):
+        textfield_id.focus = True
+
+    def total_sum(self, notes: list, total_label_id):
+        total = sum(int(textfield.text) for textfield in notes)
+        total_label_id.text = str(total)
+
+
+# launch the app
+if __name__ == "__main__":
+    Main().run()
