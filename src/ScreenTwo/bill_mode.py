04ce12b32ac8694ca1e705a2c4e300d382ed3841
Jeremiah Akpor <JeremiahAkpor_CS@outlook.com>
2023-05-15 11:01:10 +0200

    added lib used to buildozer and delete nav bar

--- src/ScreenTwo/bill_mode.py
+++ src/ScreenTwo/bill_mode.py
@@ -13,19 +13,10 @@ Methods:
 -------
 None
 """
-
-
-from kivymd.uix.behaviors import CommonElevationBehavior
-from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.screen import MDScreen
-
 from apputils import load_kv
 
 load_kv(__name__)
-
-
-class NavBar(CommonElevationBehavior, MDFloatLayout):
-    pass
 
 
 class BillMode(MDScreen):
